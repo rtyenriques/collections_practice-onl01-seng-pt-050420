d0e300be974a4d14abcc7c1a831daaa709b1d9e7
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-14 01:11:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |left, right|
+   right <=> left
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
